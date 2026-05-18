4ab8aeddcf767c15d3d2d7b7f9bf01ea57e20268
crazyguy-67 <crazy2hell67@gmail.com>
2026-05-18 15:42:59 +0530

    async reading of files

--- 3-week/index.js
+++ 3-week/index.js
@@ -1,7 +1,21 @@
 const fs = require("fs");
 
-const contents = fs.readFileSync("a.txt", "utf-8");
-console.log(contents);
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-const contents2 = fs.readFileSync("b.txt", "utf-8");
-console.log(contents2);
+  console.log(contents);
+});
+
+fs.readFile("b.txt", "utf-8", function (err, contents2) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(contents2);
+});
+
+console.log("Done starting file reads");
